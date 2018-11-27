9a1f380bae0d1698581f33fb3925b44e55d7eba9
Malik Godwin Onimisi <despeauxz@gmail.com>
2018-11-27 23:59:11 +0100

    Created the create parcel route with validations

--- server/lib/controllers/ParcelController.js
+++ server/lib/controllers/ParcelController.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+import moment from 'moment';
 import parcels from '../seeders/parcel';
 
 /**
@@ -5,6 +7,48 @@ import parcels from '../seeders/parcel';
  * @class ParcelController
  */
 class ParcelController {
+  /**
+   * Create a new parcel
+   * @method createParcel
+   * @memberof ParcelController
+   * @param {object} req
+   * @param {object} res
+   * @returns {(function|object)} Function next() or JSON object
+   */
+  static createParcel(req, res) {
+    const {
+      title,
+      weight,
+      from,
+      name,
+      digit,
+      address,
+    } = req.body;
+
+    const parcel = {
+      id: parcels.length + 1,
+      title,
+      weight,
+      from,
+      receiver: {
+        name,
+        digit,
+        address,
+      },
+      status: 'In transit',
+      createdAt: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
+      deliveredAt: moment().add(5, 'days').format('dddd, MMMM Do YYYY, h:mm:ss a'),
+    }
+
+    parcels.push(parcel);
+		res.json({
+			status: 200,
+			message: 'Parcel successfully created',
+			data: parcel,
+		});
+  }
+
+
   /**
    * Returns a list of Meal Options
    * @method getParcels
