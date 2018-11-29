4a562177c9bab07104c180776d76fbe720395ef4
Malik Godwin Onimisi <despeauxz@gmail.com>
2018-11-29 01:42:09 +0100

    Restructured ftest directory for ease

---
+++ server/tests/routes/parcels/getParcels.js
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../../../lib/app';
+
+describe('Parcel Routes: Get all parcels', () => {
+    it('should get all parcels', (done) => {
+        request(app)
+            .get('/api/v1/parcels')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.data).to.be.a('array');
+            done();
+            });
+    });
+});
+
