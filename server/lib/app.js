3dbea775ad47fe94afe884be40ccdf1059670519
Malik Godwin Onimisi <despeauxz@gmail.com>
2018-12-01 11:13:59 +0100

    imported the express session and cookie parser

--- server/lib/app.js
+++ server/lib/app.js
@@ -4,6 +4,8 @@ import logger from 'morgan';
 import cors from 'cors';
 import { config } from 'dotenv';
 import bodyParser from 'body-parser';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import apiRoutes from './routes';
 
 config();
@@ -15,7 +17,13 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
 app.use('/api', apiRoutes);
 
 const port = process.env.PORT || 8000;
